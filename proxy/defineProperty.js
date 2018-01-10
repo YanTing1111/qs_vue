cf2270b048b6b129ff31c7a995f3563bba100e43
YanTing1111 <1178170499@qq.com>
2018-01-10 15:22:20 +0800

    'Property的被代理或被劫持'

--- proxy/defineProperty.js
+++ proxy/defineProperty.js
@@ -1,13 +1,22 @@
-// var a = {b:123};
+var a = {};//被代理或被劫持
 
 // Object.defineProperty的作用，让大家精细化地操作对象的属性访问
 // Object.defineProperty有三个参数：①目标对象、②需要定义的属性或方法的名字、
                                 // ③目标属性所拥有的特性descriptor
-var a = {};
+// var a = {b:123};
+// Object.defineProperty(a,"b",{
+//     value:123,
+//     writable:true,
+//     configurable:false
+// });
+// console.log(a.b);
+// console.log(a.b = 234 , a.b);
+
 Object.defineProperty(a,"b",{
-    value:123,
-    writable:true,
-    configurable:false
+    value:3345,
+    enumerable:false
 });
-console.log(a.b);
-console.log(a.b = 234 , a.b);
+for(key in a){
+    console.log(key);
+}
+console.log(Object.keys(a));
