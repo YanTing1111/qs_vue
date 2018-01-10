892855bf82acb216e74baeeb4a2a92a8bc93d57a
YanTing1111 <1178170499@qq.com>
2018-01-10 15:57:06 +0800

    'set与get完整版'

--- proxy/defineProperty.js
+++ proxy/defineProperty.js
@@ -23,12 +23,17 @@ var a = {};//被代理或被劫持
 
 Object.defineProperty(a,"b",{
     set:function(newValue){
+        this.value = 1;
         console.log('你要赋值给我，我的新值是 ： ' + newValue);
+        console.log(`以前的值是${this.value}`);
+        this.value = newValue;
+        console.log(`新的值是${this.value}`);
     },
     get:function(){
         console.log('你取我的值');
-        return 1;
+        return this.value;
     }
 });
 console.log(a.b);
-console.log(a.b = 2);
+a.b = 2;
+console.log(a.b );
