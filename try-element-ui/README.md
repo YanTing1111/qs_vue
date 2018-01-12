5fb7506ca2102a1d11d818f1e3bc61dc979b0ede
YanTing1111 <1178170499@qq.com>
2018-01-12 21:19:31 +0800

    'element-ui入门'

---
+++ try-element-ui/README.md
@@ -0,0 +1,27 @@
+# try-element-ui
+element-ui elem PC
+Bootstrap 
+element-ui经常用于PC网站和后台界面  
+有了mvvm之后，整个网站都是组件化搭积木
+
+
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ try-element-ui/src/pages/activePublic/step1.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="step1">
+    <el-form class="demo-ruleFrom" ref="ruleFrom" label-position="top" :model="ruleFrom">
+      <el-from-item label="活动名称" prop="name">
+        <el-input v-model="ruleFrom.name" size="large"></el-input>
+      </el-from-item>
+      <el-from-item label="" prop="fenLei">
+        <el-row style="height:35px;" type="flex" align="middle">
+          <el-col :span="3" style="width:90px;">
+            <div class="el-form-item__label">活动分类</div>
+          </el-col>
+          <el-col :span="2">
+            <el-button type="text" @click.native="dialogFromFenLeiVisble=true">
+              设置
+            </el-button>
+          </el-col>
+        </el-row>
+        <el-radio-group v-model="ruleFrom.fenLei">
+          <el-radio v-for="item of ruleFrom.fenLeis" :label="item.name" :key="item.name">
+          </el-radio>
+        </el-radio-group>
+      </el-from-item>
+    </el-form>
+    <el-dialog title="设置活动分类" :visible.sync="dialogFromFenLeiVisble">
+      <el-from>
+        <el-form-item>
+          <el-tag v-for="fenLei of ruleFrom.fenLeis" type="primary" :key="fenLei" :closable="true">
+            {{fenLei.name}}
+          </el-tag>
+        </el-form-item>
+      </el-from>
+    </el-dialog>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      dialogFromFenLeiVisble:false,
+      ruleFrom:{
+        name:'',
+        fenLeis:[
+          {name:'未发布'},
+          {name:'测试活动'},
+          {name:'精彩活动'},
+        ],
+        fenLei:'测试活动',
+        tags:[]
+      }
+    }
+  }
+}
+</script>
+<style>
+
+</style>
