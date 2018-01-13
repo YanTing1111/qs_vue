eb917c27b52941b725fed74adc71d25b39b92009
YanTing1111 <1178170499@qq.com>
2018-01-13 19:36:16 +0800

    'element-ui源码'

---
+++ router-post/src/components/ElButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <button class="el-button" @click="handleClick" 
+  :class="[type?'el-button--' + type :'' , 
+  buttonSize?'el-button--' + buttonSize : '',
+  {'is-disabled':disabled}]"
+  :type="nativeType" :disabled="disabled">
+      <span v-if="$slots.default">
+          <slot></slot>
+      </span>
+  </button>
+</template>
+<script>
+export default {
+  props:{
+      disabled:Boolean,
+      nativeType:{
+          type:String,
+          default:'button'
+      },
+      size:String,
+      type:{
+         type:String,
+         default:'default' 
+      }
+  },
+  computed:{
+      buttonSize(){
+          return this.size;
+      }
+  },
+  methods:{
+      handleClick(evt){
+          this.$emit('click',evt);
+      }
+  }
+}
+</script>
+<style>
+
+</style>
