387ef82fb33d7973ec9a0ef3f411b2242c778168
YanTing1111 <1178170499@qq.com>
2018-01-14 15:26:33 +0800

    '实现中英文转换'

---
+++ vue-locale/src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import i18n from './lang'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(Element,{
+  size:'medium',
+  i18n:(key,value)=>i18n.t(key,value)
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  i18n,
+  components: { App },
+  template: '<App/>'
+})
