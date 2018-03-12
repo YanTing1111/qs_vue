5391be43f78870ec728f5286ebce6039f5beb6af
YanTing1111 <1178170499@qq.com>
2018-03-12 20:06:10 +0800

    '增加md5密码加密'

--- try-nuex/server/models/index.js
+++ try-nuex/server/models/index.js
@@ -1,6 +1,7 @@
 // mongoose mongo的驱动 数据库抽象
 import mongoose from 'mongoose'
 import config from '../config'
+import md5 from 'md5'
 require('./user')
 const User = mongoose.model('User')
 const mongoUrl = `mongodb://${config.mongodb.host}:${config.mongodb.port}/
@@ -9,6 +10,7 @@ mongoose.Promise = global.Promise
 mongoose.connection.openUri(mongoUrl).once('open',async() => {
     console.log('database connect success');
     let userInfo = config.user
+    userInfo.password = md5(userInfo.password)
     let user = await User.findOne({role:'superAdmin'}).exec()
     if(!user){
         user= new User(userInfo)
